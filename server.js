442bb3f079f9a343460ac960d44f40cffe517747
mohammad-shaheryar-05 <shaheryarmohammad5@gmail.com>
2025-04-22 21:47:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var http = require('http');
-var requests=0;
-var podname= process.env.HOSTNAME;
+var requests = 0;
+var podname = process.env.HOSTNAME || 'default-hostname';  // Fallback in case HOSTNAME is undefined
 var startTime;
 var host;
+
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
   response.write("DevOps Coursework 2! | Running on: ");
   response.write(host);
   response.end(" | v=1\n");
-  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
-}
+  
+  console.log("Running On:", host, "| Total Requests:", ++requests, "| App Uptime:", (new Date() - startTime) / 1000, "seconds", "| Log Time:", new Date());
+};
+
 var www = http.createServer(handleRequest);
-www.listen(8080,function () {
-  startTime = new Date();;
-  host = process.env.HOSTNAME;
-  console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
+
+www.listen(8080, '0.0.0.0', function() {  // Ensure it listens on all interfaces
+  startTime = new Date();
+  host = process.env.HOSTNAME || 'default-hostname';  // Fallback value if not available
+  console.log("Started At:", startTime, "| Running On:", host, "\n");
 });
